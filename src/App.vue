a41148c9b264bfd09a60e3949eacdf5aa7b18cf0
Saranya Vinjarapu <saranya@cantilever.co>
2021-12-15 03:05:17 +0530

    created header component and added it to the app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="app">Hello</div>
+  <div class="app"><Header></Header></div>
 </template>
+
+<script>
+import Header from '@/components/Header';
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
 
 <style lang="scss">
 html,
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>TOKEN SWAPPING INTERFACE</header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped lang="scss">
+header {
+  padding: 15px 30px 0px 30px;
+  font-size: 35px;
+  font-family: Helvetica, sans-serif;
+  border-bottom: 2px solid #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+}
+
+header h4 {
+  text-transform: capitalize;
+  font-style: italic;
+  font-size: 30px;
+  font-family: Helvetica, sans-serif;
+  font-weight: 500;
+}
+</style>
