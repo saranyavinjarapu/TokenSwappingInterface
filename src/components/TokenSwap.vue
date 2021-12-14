7d2bf586349e73561ce48f2929401f5877b76473
Saranya Vinjarapu <saranya@cantilever.co>
2021-12-15 03:47:15 +0530

    added token swap component with necessary form fields and swap button and add the component to home view

---
+++ src/components/TokenSwap.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="tokenswap">
+    <form ref="tokenswapform" @submit.prevent="false">
+      <div>
+        <label>Choose From Token</label>
+        <select v-model="selectFromToken"></select>
+      </div>
+      <div>
+        <label>Enter Amount to Swap</label>
+        <input type="number" v-model="swapAmount" />
+      </div>
+      <div>
+        <label>Choose To Token</label>
+        <select v-model="selectToToken"></select>
+      </div>
+      <div>
+        <label>Pool Price</label>
+      </div>
+      <div>
+        <label>Expected Token Amount</label>
+      </div>
+      <br />
+      <div class="button_container">
+        <button type="submit">SWAP</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TokenSwap',
+  data() {
+    return {
+      selectFromToken: '',
+      swapAmount: '',
+      selectToToken: ''
+    };
+  }
+});
+</script>
+
+<style scoped lang="scss">
+.tokenswap {
+  padding: 20px 50px 0px 50px;
+  margin-bottom: 20px;
+  border-radius: 5px;
+  background: #d3d3d3;
+  display: flex;
+}
+
+.dark .tokenswap {
+  background: #212429;
+}
+
+.tokenswap form div {
+  margin-top: 20px;
+  margin-bottom: 30px;
+}
+.tokenswap form label {
+  display: inline-block;
+  width: 230px;
+  text-align: left;
+  font-size: 20px;
+}
+
+.tokenswap form select {
+  padding: 5px 20px 5px 20px;
+}
+
+.tokenswap form input {
+  padding: 5px 20px 5px 20px;
+  width: 55px;
+  font-size: 15px;
+  font-family: Arial, sans-serif;
+}
+
+.tokenswap .button_container {
+  justify-content: center;
+  display: flex;
+  margin-top: 0px;
+}
+.tokenswap .button_container button {
+  background: #4caf50;
+  border: none;
+  color: #000000;
+  padding: 15px;
+  border-radius: 100px;
+  width: 100px;
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+}
+
+.dark .tokenswap .button_container button {
+  background: #2172e5;
+  color: #ffffff;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <UserAddress />
+    <TokenSwap />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import UserAddress from '@/components/UserAddress.vue';
+import TokenSwap from '@/components/TokenSwap.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
-    UserAddress
+    UserAddress,
+    TokenSwap
   }
 });
 </script>
